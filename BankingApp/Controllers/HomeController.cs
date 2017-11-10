ab36c78b85e7f85a5a8465e1ac799ca04e16138f
Jenna McCarter <Jenna@mc-carter.com>
2017-11-09 19:42:32 -0800

    Adding Files

---
+++ BankingApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // Change from template code to app specific - JM
+            ViewBag.Message = "Meow Banking app for AltSource";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            // Change from template code to app specific - JM
+            ViewBag.Message = "Contact us Meow!";
+            return View();
+        }
+    }
+}
